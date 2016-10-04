e76b997aa9d5a33124a5e0782f76246aaf0b682c
Kevin Meredith <kevin.m.meredith@gmail.com>
2016-10-03 22:59:45 -0400

    add List

--- Recursive.scala
+++ Recursive.scala
@@ -2,3 +2,7 @@
 sealed trait Tree[+A]
 case object Empty                                          extends Tree[Nothing]
 case class Node[A](left: Tree[A], leaf: A, right: Tree[A]) extends Tree[A]
+
+sealed trait List[+A]
+case object EmptyList 					extends List[Nothing]
+case class Cons[A](x: A, tail: List[A]) extends List[A]
