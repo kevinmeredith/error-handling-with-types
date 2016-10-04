18dae4b0a293a5f4a5772e774a29ef837a3c2ca4
Kevin Meredith <kevin.m.meredith@gmail.com>
2016-10-03 22:32:51 -0400

    Abstraction.

---
+++ TSAbstraction.scala
@@ -0,0 +1,19 @@
+case class Person(name: String, age: Int, id: Long)
+
+trait UserRepository {
+	def get(id: Long): Option[Person]
+}
+
+case object UserRepositoryImpl extends UserRepository {
+	override def get(id: Long): Option[Long] = {
+		// get connection to DB
+		// perform look up
+		// decode SQL DB Row into a Person
+		???
+	}
+}
+
+case object UserRepositoryTestImpl extends UserRepository {
+	override def get(id: Long): Option[Person] = 
+		None
+}
